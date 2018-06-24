5d88adeab4bb0b16b7dc5fe8b9c82958811247b0
man.le <counterscience93@gmail.com>
2018-06-24 10:41:45 +0700

    up both resources

---
+++ ProjectSEM3/ProjectSEM3/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectSEM3.Dao;
+using ProjectSEM3.DAO;
+using ProjectSEM3.EF;
+
+namespace ProjectSEM3.Controllers
+{
+    public class UserController : Controller
+    {
+        UserDAO _userDao = new UserDAO();
+        // GET: User
+        public ActionResult Detail(int id)
+        {
+            var temp = _userDao.ViewDetail(id);
+            return View(temp);
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            var dao = new UserDAO();
+            var model = dao.Update(user);
+            if (model)
+            {
+                TempData["Result"] = "Cập nhập tài khoản thành công";
+                return RedirectToAction("Detail", new { id = user.ID });
+            }
+            else
+            {
+                TempData["Result"] = "Cập nhập tài khoản thất bại";
+                return RedirectToAction("Detail", new { id = user.ID });
+            }
+        }
+        public void setViewBag(long? selectedID = null)
+        {
+            var dao = new GroupUserDao();
+            ViewBag.GroupUserID = new SelectList(dao.GetAllGroupUsers(""), "ID", "Name", selectedID);
+
+        }
+
+    }
+
+}
